5097e3d285f4b32e3015a764dc0f940a7cbd359f
LAPTOP-P3BPH6JG\yoop1 <yoop1000@naver.com>
2024-02-22 16:33:29 +0900

    feat : UI구성을 위해 view( Navigation( Fragment ) ) 생성

--- app/src/main/java/com/example/movieinfo/MainActivity.kt
+++ app/src/main/java/com/example/movieinfo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.movieinfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,35 +13,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.movieinfo.ui.theme.MovieInfoTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MovieInfoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MovieInfoTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/example/movieinfo/features/detail/DetailFragment.kt
@@ -0,0 +1,27 @@
+package com.example.movieinfo.features.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material3.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import com.example.movieinfo.ui.theme.MovieInfoTheme
+
+class DetailFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                MovieInfoTheme() {
+                    Text(text = "detailFragment~")
+                }
+            }
+        }
+    }
+}
